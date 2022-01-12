fb9f843bd662dfcc0ec6ab90464aa54ea5f506d2
onlyorlov <onlyorlov@gmail.com>
2022-01-12 20:06:02 +0300

    qq

--- utils/general.py
+++ utils/general.py
@@ -126,6 +126,9 @@ def check_dataset(dict):
 
 def make_divisible(x, divisor):
     # Returns x evenly divisible by divisor
+    # Returns nearest x divisible by divisor
+    if isinstance(divisor, torch.Tensor):
+        divisor = int(divisor.max())  # to int
     return math.ceil(x / divisor) * divisor
 
 
