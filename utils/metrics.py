bea8a1a2bea628bb2cb1c8239e38d6edcd8abb07
onlyorlov <onlyorlov@gmail.com>
2022-01-12 19:19:22 +0300

    f

--- utils/metrics.py
+++ utils/metrics.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
-from general import box_iou
+from . import general.box_iou
 
 
 def fitness(x):
@@ -125,7 +125,7 @@ class ConfusionMatrix:
         detections = detections[detections[:, 4] > self.conf]
         gt_classes = labels[:, 0].int()
         detection_classes = detections[:, 5].int()
-        iou = box_iou(labels[:, 1:], detections[:, :4])
+        iou = general.box_iou(labels[:, 1:], detections[:, :4])
 
         x = torch.where(iou > self.iou_thres)
         if x[0].shape[0]:
